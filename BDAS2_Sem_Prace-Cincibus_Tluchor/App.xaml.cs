e9e8c29492de4cb27519dd0f560b5c82300dcdc7
MilanCinci <st72496@upce.cz>
2025-09-23 18:56:26 +0200

    Vytvořen prázdný projekt v WPF

---
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BDAS2_Sem_Prace_Cincibus_Tluchor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
