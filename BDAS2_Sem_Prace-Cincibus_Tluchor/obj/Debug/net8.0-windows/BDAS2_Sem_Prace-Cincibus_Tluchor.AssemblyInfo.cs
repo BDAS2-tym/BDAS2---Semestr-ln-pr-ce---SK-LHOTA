b12a3e811a2008ae480d30f973bc373843de6972
Aleš Tlučhoř <st72870@upce.cz>
2025-09-28 12:13:55 +0200

    - Okna Hráči a Trenéři se přepínají s hlavním oknem

--- BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71678788bee300c0d9d9bd1408cdc76706e817bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af61c3396df1a846880988ddea3e52e04d70dbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
