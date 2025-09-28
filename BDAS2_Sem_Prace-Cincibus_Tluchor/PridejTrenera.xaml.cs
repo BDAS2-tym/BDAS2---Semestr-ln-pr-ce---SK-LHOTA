af61c3396df1a846880988ddea3e52e04d70dbd0
Aleš Tlučhoř <st72870@upce.cz>
2025-09-28 11:29:25 +0200

    - Okno Hráči a Trenéři dodělané design - Funguje přechod mezi oknami Hráči, Trenéři a HlavníOkno

--- BDAS2_Sem_Prace-Cincibus_Tluchor/PridejTrenera.xaml.cs
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/PridejTrenera.xaml.cs
@@ -1,25 +1,13 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BDAS2_Sem_Prace_Cincibus_Tluchor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    
+    public partial class PridejTrenera : Window
     {
-        public MainWindow()
+        public PridejTrenera()
         {
             InitializeComponent();
-            
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
