c4d6404699a1864d2bc1a12f19e3716510f44aec
MilanCinci <st72496@upce.cz>
2025-09-30 11:26:07 +0200

    Předělán design okna TreneriOkno (tabulka + tlačítka)

--- BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36771fb9aa22cfd8f26f213e7b13661cfe44e28f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62477d1be54f55d5323f7bd71d26243b74b96b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
