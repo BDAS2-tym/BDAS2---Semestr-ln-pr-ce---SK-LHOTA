b3094303960da935e454c7c10ed5fb9b156b83cd
MilanCinci <st72496@upce.cz>
2025-10-09 16:37:49 +0200

    Změněno přihlašovací okno

--- BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92fac8d457a796b5d75ef067c05c0dbe156b2155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95855eedf6418b4e60afebc8e24fa115213ea48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
