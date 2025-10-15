d14dd32ebc3a26327f78412acc7024579cdaa528
MilanCinci <st72496@upce.cz>
2025-10-15 09:43:54 +0200

    Přidáno okno (design): DialogVazebniTabulkaClenove

--- BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
+++ BDAS2_Sem_Prace-Cincibus_Tluchor/obj/Debug/net8.0-windows/BDAS2_Sem_Prace-Cincibus_Tluchor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd27ec54145d067c504ec640b6bb130da6bcdbdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c4bca22fc9fc035cc50dcecbbc6aee7f132c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BDAS2_Sem_Prace-Cincibus_Tluchor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
